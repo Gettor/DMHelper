25085c89def4f387f4223dff0b2a484807da0eb3
Slawomir Dobroc <sdobroc@gmail.com>
2017-08-25 15:43:40 +0200

    Made move'able dot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 export class Segment {
     id: number;
@@ -23,6 +24,15 @@ const SEGMENTS: Segment[] = [
     {id: 8, xPos: "200px", yPos: "400px"}
 ];
 
+function move(x, y) {
+    //console.log(div_pc.style.top);
+    let div_pc = document.getElementById("pc_dot");
+    let old_x = parseInt(div_pc.style.left);
+    let old_y = parseInt(div_pc.style.top);
+    div_pc.style.left = String(old_x + x) + "px";
+    div_pc.style.top = String(old_y + y) + "px";
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -36,4 +46,26 @@ export class AppComponent {
         xPos: "133px",
         yPos: "333px"
     };
+
+
+    @HostListener('document:keypress', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        console.log(event.key);
+        switch(event.key){
+            case "a":
+                move(-100, 0);
+                break;
+            case "s":
+                move(0, 100);
+                break;
+            case "d":
+                move(100, 0);
+                break;
+            case "w":
+                move(0, -100);
+                break;
+            default:
+        }
+    }
+
 }
