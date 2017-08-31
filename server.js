509d7a5bb93c135c5801c90e5de7b3daf9f81fa2
Gettor <sdobroc@gmail.com>
2017-08-31 13:23:21 +0200

    Server listens for simple GET request to return db records

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/d2db";
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("segments").find({}).toArray(function(err, result) {
+            res.send(JSON.stringify(result));
+            db.close();
+        });
+    });
+});
  
 app.listen(3000);
